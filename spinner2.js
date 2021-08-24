2e053ec7bc72ee36ffaaf76d308878de2e0db4dc
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-24 19:55:16 +0000

    added new line at the end of the loop

--- spinner2.js
+++ spinner2.js
@@ -4,6 +4,9 @@ let timer = 100;
 for (let i = 0; i < array.length; i++) {
   setTimeout(() => {
     process.stdout.write(array[i]);
+    if (i === array.length - 1) {
+      process.stdout.write('\n');
+    }
     timer += 200;
   }, timer)
   timer += 200;
